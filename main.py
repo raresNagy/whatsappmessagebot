cabd0980861e2e551d33c9181e48ef8e64131914
Rares Nagy <rares.nagy@gmail.com>
2025-06-04 14:44:38 +0300

    First working version

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+
 options = webdriver.FirefoxOptions()
 driver = webdriver.Firefox(options=options)
 driver.implicitly_wait(10) # seconds
@@ -24,13 +25,34 @@ new_message_button = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/bod
 sleep(0.7)
 
 for number in phone_list:
-    new_message_button.click()
+    driver.execute_script("arguments[0].scrollIntoView(true);", new_message_button)
+    try:
+        new_message_button.click()
+    except Exception:
+        driver.execute_script("arguments[0].click();", new_message_button)
 
     search_bar = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div[3]/div/div[2]/div[1]/span/div/span/div/div[1]/div[2]/div/div")))
 
-    sleep(0.1)
-    search_bar.click()
     sleep(0.5)
+
+    search_bar.click()
     for char in number.strip():
-        search_bar.send_keys(char)
-        sleep(0.1)
+        driver.switch_to.active_element.send_keys(char)
+    sleep(0.5)  # Wait for contacts to appear
+    # Use the fallback XPath for the first search result
+    first_result = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[@title][@dir='auto']/ancestor::div[@tabindex='0']")))
+    print("Clicking first contact result: ", first_result.text)
+    first_result.click()
+    # Wait for the message input box
+    message_box = wait.until(EC.element_to_be_clickable((By.XPATH, "//footer//div[@contenteditable='true']")))
+    # Read the message from message.txt
+    with open("message.txt", "r") as msg_file:
+        message = msg_file.read().strip()
+    # Send the message
+    message_box.click()
+    for char in message:
+        message_box.send_keys(char)
+    # Click the send button directly instead of pressing Enter
+    send_button = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div/div[3]/div/div[4]/div/footer/div[1]/div/span/div/div[2]/div/div[4]/button")))
+    send_button.click()
+    sleep(0.5)
